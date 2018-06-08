e134e8579e9afda3d2020998c8f7d3e6b3f5b3c1
Dave Leigh <aftershok@gmail.com>
2018-06-08 23:31:10 +0100

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       resolve: 'gatsby-source-contentful', 
       options: {
         spaceId: 'c20vnmipwf84',
-        accessToken: '623b9a2888e8a2662e1967b1b589adecec75173b0efd408ad9b27981a462541a'
+        accessToken: ''
       }
     },
     'gatsby-plugin-react-helmet'
